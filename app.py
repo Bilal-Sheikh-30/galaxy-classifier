7e74d16642975829c6f5b715ea7ffad5bf6e3d9b
Bilal-Sheikh-30 <bilalsheikh3011@gmail.com>
2025-08-06 23:38:43 +0500

    Fix: Set dynamic port for Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, url_for, jsonify, request
 from PIL import Image
 import io, base64
@@ -79,4 +80,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
